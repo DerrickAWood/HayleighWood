3871bc20d5fa2665bf67ced296e534fbaf3fa138
Derrick A Wood <dwood_1994@yahoo.com>
2020-06-09 10:59:15 -0600

    contact info

---
+++ main.js
@@ -0,0 +1,13 @@
+function facebook(){
+  window.open("https://www.facebook.com/woodworksphotography2019/")
+}
+
+function email(){
+  // @ts-ignore
+  swal("woodwooksphotography@outlook.com");
+}
+
+function cell(){
+  // @ts-ignore
+  swal("(808) 343-7875");
+}
